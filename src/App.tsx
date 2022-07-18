883dab15329235da7a0e91b6ca8caddbb4f1bf10
Sergio0831 <sergejs.ivcenko@gmail.com>
2022-07-18 15:17:32 +0100

    Created folder structure

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Route, Routes } from 'react-router-dom';
+import FavoritesPage from './pages/FavoritesPage';
+import HomePage from './pages/HomePage';
+
+function App() {
+  return (
+    <Routes>
+      <Route path='/' element={<HomePage />} />
+      <Route path='/favorites' element={<FavoritesPage />} />
+    </Routes>
+  );
+}
+
+export default App;
+
